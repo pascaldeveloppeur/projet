5870d55730c3271e6566e5ab1573d281298b2ac2
pascaldeveloppeur <pascalyakouyami@gmail.com>
2023-11-23 14:36:48 +0100

    u

--- member-service/src/main/java/org/dev/repository/MemberRepo.java
+++ member-service/src/main/java/org/dev/repository/MemberRepo.java
@@ -7,7 +7,7 @@ import org.dev.model.MemberType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
+
 public interface MemberRepo extends JpaRepository<Member, Long> {
 	
 	
