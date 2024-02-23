29954424d967bba1ed09c258d53a3d6a87834149
pascalDev <pascalyakouyami@gmail.com>
2024-02-23 13:52:20 +0100

    u

---
+++ dev/member-registration-service/src/main/java/com/pascaldev/memberregistrationservice/MemberRegistrationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pascaldev.memberregistrationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberRegistrationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberRegistrationServiceApplication.class, args);
+	}
+
+}
---
+++ dev/member-registration-service/src/test/java/com/pascaldev/memberregistrationservice/MemberRegistrationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pascaldev.memberregistrationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberRegistrationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
