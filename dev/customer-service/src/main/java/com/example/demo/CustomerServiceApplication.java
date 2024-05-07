7caaca430adb7241649a35c775ce4e8c96473644
pascalDev <pascalyakouyami@gmail.com>
2024-05-07 15:35:03 +0100

    u

--- dev/customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
+++ dev/customer-service/src/main/java/com/example/demo/CustomerServiceApplication.java
@@ -1,14 +1,19 @@
 package com.example.demo;
 
+import java.util.List;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 import com.example.demo.entities.Customer;
+import com.example.demo.repository.CustomerRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @Slf4j
 public class CustomerServiceApplication {
@@ -17,7 +22,28 @@ public class CustomerServiceApplication {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 		log.info("Customer service started!");
 		
-		
+	
 	}
-
+	
+	@Bean
+	CommandLineRunner commandLineRunner(CustomerRepository customerRepo) {
+		
+			return args ->{
+				List<Customer> customerList= List.of(
+						Customer.builder()
+							.firstName("pascal")
+							.lastName("Yakou")
+							.email("pascalyakouyami@gmail.com")
+							.build(),
+							Customer.builder()
+							.firstName("ima")
+							.lastName("Sape")
+							.email("ima@gmail.com")
+							.build() 
+						
+						);
+				customerRepo.saveAll(customerList);
+						
+			};
+	}
 }
--- dev/customer-service/src/main/java/com/example/demo/entities/Customer.java
+++ dev/customer-service/src/main/java/com/example/demo/entities/Customer.java
@@ -4,10 +4,18 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Customer")
 public class Customer {
 
 	@Id
